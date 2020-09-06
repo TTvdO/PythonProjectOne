7a3399caa7f213ac5857349d63fed5242b8c5a11
Tim <17072565@student.hhs.nl>
2020-09-06 16:29:47 +0200

    need to make the grid in code and base the drawing off of the existing grid

--- Grid.py
+++ Grid.py
@@ -7,39 +7,25 @@ class Grid:
         self.screenWidthAndHeight = screenWidthAndHeight
         self.rowsAndColumns = rowsAndColumns
         self.roomPerBlock = self.screenWidthAndHeight / self.rowsAndColumns
-        self.blockWidthAndHeight = self.roomPerBlock * 0.9
-        self.blockMarginLeft = self.roomPerBlock * 0.05
-        self.blockMarginRight = self.roomPerBlock * 0.05
+
+        self.create_in_memory_grid()
+
+        # unused for now
+        # self.blockWidthAndHeight = self.roomPerBlock * 0.9
+        # self.blockMarginLeft = self.roomPerBlock * 0.05
+        # self.blockMarginRight = self.roomPerBlock * 0.05
     
-    def draw_grid(self):
-        # TOCHANGE: i.p.v. op basis v.d. screenWidthAndHeight loopen, overgaan naar op basis v.d. opgegeven rowsAndColumns loopen
+    def create_in_memory_grid(self):
+        pass
+
+    # TOCHANGE: should be based off of an existing grid that you can use in memory. not just a drawing based on nothing
+    # also will need to update this grid after every change later on. can do this by calling draw_grid at the end of the while loop in main, but there should be a better way
+    # to refresh only the parts of the grid that changed (without refreshing the entirety of the grid)
+    def draw_grid(self, screen):
         for column in range(self.rowsAndColumns):
-            # TOCHANGE: i.p.v. op basis v.d. screenWidthAndHeight loopen, overgaan naar op basis v.d. opgegeven rowsAndColumns loopen
             for row in range(self.rowsAndColumns):
-                # hmm
                 rect = pygame.Rect(row * self.roomPerBlock, column * self.roomPerBlock, self.roomPerBlock, self.roomPerBlock)
                 pygame.draw.rect(screen, WHITE, rect, 1)
-
-    def draw_grid_old(self, screen):
-        # TOCHANGE: i.p.v. op basis v.d. screenWidthAndHeight loopen, overgaan naar op basis v.d. opgegeven rowsAndColumns loopen
-        for column in range(self.rowsAndColumns):
-            # TOCHANGE: i.p.v. op basis v.d. screenWidthAndHeight loopen, overgaan naar op basis v.d. opgegeven rowsAndColumns loopen
-            for row in range(self.rowsAndColumns):
-                # hmm
-                rect = pygame.Rect(row * self.roomPerBlock, column * self.roomPerBlock, self.roomPerBlock, self.roomPerBlock)
-                pygame.draw.rect(screen, WHITE, rect, 1)
-
-    def draw_grid_older(self, SCREEN, GRID_BLOCK_SIZE):
-        for column in range(self.screenWidthAndHeight):
-            for row in range(self.screenWidthAndHeight):
-                rect = pygame.Rect(column * GRID_BLOCK_SIZE, row * GRID_BLOCK_SIZE, GRID_BLOCK_SIZE, GRID_BLOCK_SIZE)
-                pygame.draw.rect(SCREEN, WHITE, rect, 1)
-
-    def draw_grid_newer(self, SCREEN, GRID_BLOCK_SIZE):
-        for column in range(self.rowsAndColumns):
-            for row in range(self.rowsAndColumns):
-                rect = pygame.Rect(row * GRID_BLOCK_SIZE, column * GRID_BLOCK_SIZE, GRID_BLOCK_SIZE, GRID_BLOCK_SIZE)
-                pygame.draw.rect(SCREEN, WHITE, rect, 1)
 
     def get_screen():
         return screen
--- main.py
+++ main.py
@@ -26,10 +26,8 @@ def main():
     # infinite loop for the program to run in until user exits program
     while 1:
         # draw the grid first, with x amount of rows and columns and with different type of terrain in the future, with their own amount of points to move over
-        grid = Grid(SCREEN_WIDTH_AND_HEIGHT, 10)
-        grid.draw_grid_old(SCREEN)
-        # grid.draw_grid_newer(SCREEN, GRID_BLOCK_SIZE)
-        # draw_grid(SCREEN)
+        grid = Grid(SCREEN_WIDTH_AND_HEIGHT, 25)
+        grid.draw_grid(SCREEN)
 
         # monitor events
         for event in pygame.event.get():
@@ -38,15 +36,6 @@ def main():
         
         # update screen based on inputs
         pygame.display.flip()
-
-def draw_grid(SCREEN):
-    # voor elke kolom, tot en met 240 pixels
-    for column in range(SCREEN_WIDTH_AND_HEIGHT):
-        # door de row van deze kolom heengaan, tot en met 320 pixels
-        for row in range(SCREEN_WIDTH_AND_HEIGHT):
-            # voor elk stukje van deze row een vierkant tekenen met als linkerbegin de 
-            rect = pygame.Rect(column * GRID_BLOCK_SIZE, row * GRID_BLOCK_SIZE, GRID_BLOCK_SIZE, GRID_BLOCK_SIZE)
-            pygame.draw.rect(SCREEN, WHITE, rect, 1)
 
 if __name__ == '__main__':
     main()
