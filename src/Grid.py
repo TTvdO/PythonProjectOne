b3417396e8d939c363e089c2e6bd9a0aeb7ad97a
Tim <17072565@student.hhs.nl>
2020-09-12 08:52:48 +0200

    remove comments

--- src/Grid.py
+++ src/Grid.py
@@ -17,18 +17,12 @@ class Grid:
         self.hasStartingPoint = False
         self.hasEndPoint = False
         self.amountOfBlocksCreated = 0
-        self.amountOfTimesCalled = 0
 
         self.create_in_memory_grid()
 
     def create_in_memory_grid(self):
-        # je kan hier het best de x en y doorgeven. als dat niet kan met deze syntax, moet je hier een dubbele for loop van maken om de x en y door te kunnen geven
-        # aan de self.get_random_block() method
         self.grid = [[self.get_random_block(x, y) for x in range(self.rowsAndColumns)] for y in range(self.rowsAndColumns)]
-
-    # within this method, you want to not only create a certain Terrain, but you want to give an X and Y value to the terrain created in the constructor.
-    # so keep track of the self.rowsAndColumns attribute and make sure that the X and Y values are correctly updated throughout the method.
-    # might need help from create_in_memory_grid
+        
     def get_random_block(self, x, y):
         randomNumber = random.randrange(1, 48, 1)
         # Create starting point at one index before the last if no starting point has been created yet before then
@@ -58,7 +52,6 @@ class Grid:
             else:
                 block = Ground(x, y)
         self.amountOfBlocksCreated+=1
-        self.amountOfTimesCalled+=1
         return block
 
     def get_room_per_block(self):
