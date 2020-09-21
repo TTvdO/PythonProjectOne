5b7bfe93aa4feb70c94a35771d090e773b7cc4ef
Tim <tim.oest@live.nl>
2020-09-21 15:56:34 +0200

    beginning structure astar algorithm

---
+++ src/traversal_algorithms/AStar.py
@@ -0,0 +1,14 @@
+from .TraversalAlgorithm import TraversalAlgorithm
+
+class AStar(TraversalAlgorithm):
+    def __init__(self, gridClass, draw):
+        super().__init__(gridClass, draw)
+
+    def initialize(self):
+        pass
+
+    def move(self):
+        pass
+
+    def backtrack(self):
+        pass
